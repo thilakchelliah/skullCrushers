c744436d3190ecfb04aef8929c3a4aae36a1563f
thilakchelliah <thilak.chelliah49@gmail.com>
2019-04-11 11:01:59 +0000

    hj

--- ClientCode/Angular1/Directives/MailManager/Mail.directive.js
+++ ClientCode/Angular1/Directives/MailManager/Mail.directive.js
@@ -8,12 +8,16 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
             $scope.message = [];
             $scope.indMessaArray = [];
             var init = function () {
-                // debugger;
+                $scope.create = false;
                 DMService.GetMailList().then(
                     function (response) {
                         // debugger;
-                        $scope.message=response.data.messages;
-                        $scope.loaded=true;
+                        $scope.message = response.data.messages;
+                        $scope.frm = {};
+                        $scope.frm.msg = "";
+                        $scope.frm.sub = "";
+                        $scope.frm.to = "";
+                        $scope.loaded = true;
                         console.log(response)
                         // $scope.mailcontent = $sce.trustAsHtml($scope.message[1].body.content);
                         $scope.sub = $scope.message[0].subject;
@@ -24,7 +28,7 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
                         console.log($scope.to[0].emailAddress)
                         $scope.mailcontent = $scope.message[0].body.content.replace(new RegExp('\n', 'g'), "<br />")
                         // $scope.mailcontent = $scope.message[1].body.content;
-                        
+
 
                         // $.each($scope.message, function (key, value) {
                         //         let toneParams = {
@@ -34,7 +38,7 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
                         //     DMService.ToneAnalyse(toneParams).then(
                         //         function (tonResp) {                                    
                         //             $scope.indMessaArray.push({ emailId: value.id, result: tonResp });
-                                    
+
                         //             console.log(tonResp)
                         //         },
                         //         function (err) {
@@ -49,10 +53,40 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
 
                     });
             }
-            $scope.body_click = function(a){
+            $scope.create_click = function () {
+                $scope.create = true;
+            }
+            $scope.body_click = function (a) {
+                $scope.create = false;
                 $scope.$broadcast('BOOM!', a)
             }
-            $scope.$on('BOOM!', function(events, args){
+            $scope.discard = function () {
+                $scope.create = false;
+            }
+            $scope.send = function (a) {
+                if (a.msg == "" || a.sub == "" || a.to == "") {
+                    alert("Please fill the mandatory fields")
+                }
+                else {
+                    let toneParams1 = {
+                        tone_input: a,
+                        content_type: 'text/plain'
+                    };
+                    DMService.ToneAnalyse(toneParams1).then(
+                        function (tonResp) {
+                            console.log(tonResp)
+                            $scope.contentEmotions = tonResp.data.document_tone.tones;
+                            $scope.emarray = [];
+                            $.each($scope.contentEmotions, function (key, value) {
+                                $scope.emarray.push(value.tone_name)
+                            });
+                        },
+                        function (err) {
+
+                        });
+                }
+            }
+            $scope.$on('BOOM!', function (events, args) {
                 console.log(args);
                 $scope.sub = args.subject;
                 $scope.from = args.from.emailAddress.name;
@@ -60,10 +94,10 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
                 $scope.from_email = args.from.emailAddress.address;
                 $scope.to = args.toRecipients;
                 // $scope.to.email = args.toRecipients[0].emailAddress.address;
-                
+
 
                 $scope.mailcontent = args.body.content.replace(new RegExp('\n', 'g'), "<br />");
-              })
+            })
             init();
             $scope.openTutorial = function (id) {
                 debugger;
