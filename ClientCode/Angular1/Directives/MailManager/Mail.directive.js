cfa03ebcb286c60b757f34fe28906be1ca8893a2
thilakchelliah <thilak.chelliah49@gmail.com>
2019-04-11 14:23:14 +0000

    sd

--- ClientCode/Angular1/Directives/MailManager/Mail.directive.js
+++ ClientCode/Angular1/Directives/MailManager/Mail.directive.js
@@ -25,32 +25,41 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
                         // $scope.mailcontent = $sce.trustAsHtml($scope.message[1].body.content);
                         $scope.sub = $scope.message[0].subject;
                         $scope.from = $scope.message[0].from.emailAddress.name;
-                        // $scope.firs = $scope.from.charAt(0);
+                        
                         $scope.from_email = $scope.message[0].from.emailAddress.address;
                         $scope.to = $scope.message[0].toRecipients;
                         console.log($scope.to[0].emailAddress)
                         $scope.mailcontent = $scope.message[0].body.content.replace(new RegExp('\n', 'g'), "<br />")
                         // $scope.mailcontent = $scope.message[1].body.content;
 
+                        console.log($scope.message[0]);
+                        $.each($scope.message, function (key1, value1) {
+                                let toneParams = {
+                                    tone_input: value1.body.content + value1.subject,
+                                    content_type: 'text/plain'
+                                };
+                            DMService.ToneAnalyse(toneParams).then(
+                                function (tonResp) {
+                                    var temp_array = [];             
+                                    $.each(tonResp.data.document_tone.tones, function (key, value) {
+                                        if (value.tone_name === "fear") {
+                                            temp_array.indexOf("Sadness") === -1 ? temp_array.push("Sadness") : console.log("Sadness already exists");
+                                        } else if (value.tone_name === "Joy" || value.tone_name === "Analytical") {
+                                            temp_array.indexOf("Good") === -1 ? temp_array.push("Good") : console.log("Good already exists");
+                                        } else {
+                                            temp_array.indexOf(value.tone_name) === -1 ? temp_array.push(value.tone_name) : console.log(value.tone_name + " already exists");
+                                        }
+                                    });                       
+                                    $scope.message[key1].result = temp_array;
+                                },
+                                function (err) {
 
-                        // $.each($scope.message, function (key, value) {
-                        //         let toneParams = {
-                        //             tone_input: value.body.content,
-                        //             content_type: 'text/plain'
-                        //         };
-                        //     DMService.ToneAnalyse(toneParams).then(
-                        //         function (tonResp) {                                    
-                        //             $scope.indMessaArray.push({ emailId: value.id, result: tonResp });
-
-                        //             console.log(tonResp)
-                        //         },
-                        //         function (err) {
-
-                        //         });
-                        // });
-                        // console.log($scope.indMessaArray)
-
-
+                                });
+                        });
+                        console.log("Anand")
+                        // console.log($scope.message)
+                        $scope.emrel = $scope.message[0].result;
+                        alert($scope.emrel)
                     },
                     function (err) {
 
@@ -118,10 +127,11 @@ DMApp.directive('mailDirective', ['$localStorage', function ($localStorage) {
                 }
             }
             $scope.$on('BOOM!', function (events, args) {
+                // console.log("BOOM")
                 console.log(args);
                 $scope.sub = args.subject;
                 $scope.from = args.from.emailAddress.name;
-                // $scope.firs = $scope.from.charAt(0);
+                $scope.emrel = args.result;
                 $scope.from_email = args.from.emailAddress.address;
                 $scope.to = args.toRecipients;
                 // $scope.to.email = args.toRecipients[0].emailAddress.address;
